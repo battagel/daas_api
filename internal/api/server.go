74c3e3202138c2e10bd1a31be838d081ed2d07b1
battagel <matthew.battagel@gmail.com>
2024-03-22 18:30:44 +0000

    Self Signed Certificate

--- internal/api/server.go
+++ internal/api/server.go
@@ -5,6 +5,8 @@ import (
 	"daas_api/internal/phrase"
 	"daas_api/pkg/logger"
 	"net/http"
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -17,45 +19,53 @@ type PhraseDatabase interface {
 }
 
 type Server struct {
-	logger logger.Logger
-	ctx 	context.Context
-    Router *gin.Engine
-    srv    *http.Server
-	pdb PhraseDatabase
+	logger   logger.Logger
+	ctx      context.Context
+	Router   *gin.Engine
+	srv      *http.Server
+	pdb      PhraseDatabase
 	certFile string
-	keyFile string
+	keyFile  string
 }
 
 func CreateAPIServer(logger logger.Logger, ctx context.Context, mode, addr, certFile, keyFile string, pdb PhraseDatabase) (*Server, error) {
-    // Create a new Gin router
-    if mode == "release" {
+	// Create a new Gin router
+	if mode == "release" {
 		gin.SetMode(gin.ReleaseMode)
 	}
-    router := gin.Default()
+	router := gin.Default()
 
-    // Apply CORS middleware
-    config := cors.DefaultConfig()
-    config.AllowOrigins = []string{"https://stackoverflow.com", "http://confluence.eng.nimblestorage.com", "https://rndwiki-pro.its.hpecorp.net"}
-    config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
-    config.AllowHeaders = []string{"Content-Type", "Authorization"}
+	// Apply CORS middleware
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"https://stackoverflow.com", "http://confluence.eng.nimblestorage.com", "https://rndwiki-pro.its.hpecorp.net"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	config.AllowHeaders = []string{"Content-Type", "Authorization"}
 	config.AllowCredentials = true // Allow credentials in cross-origin requests
-    router.Use(cors.New(config))
+	router.Use(cors.New(config))
 
-    return &Server{
-        logger: logger,
-        ctx:    ctx,
-        Router: router,
-        srv:    &http.Server{Addr: addr, Handler: router},
-        pdb:    pdb,
+	srv := &http.Server{
+		Addr:           addr,
+		Handler:        router,
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	return &Server{
+		logger:   logger,
+		ctx:      ctx,
+		Router:   router,
+		srv:      srv,
+		pdb:      pdb,
 		certFile: certFile,
-		keyFile: keyFile,
-    }, nil
+		keyFile:  keyFile,
+	}, nil
 }
 
 func (s *Server) Start() {
-    if err := s.srv.ListenAndServeTLS(s.certFile, s.keyFile); err != http.ErrServerClosed {
-        s.logger.Errorw("Server error: %v\n", err)
-    }
+	if err := s.srv.ListenAndServeTLS(s.certFile, s.keyFile); err != http.ErrServerClosed {
+		s.logger.Errorw("Server error: %v\n", err)
+	}
 }
 
 func (s *Server) Stop() error {
